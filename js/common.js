a6624e398d32c4c83bbf529d79b5275f44335530
imzfly05 <5050503n@gmail.com>
2018-12-04 16:48:21 +0300

    bator

--- js/common.js
+++ js/common.js
@@ -1,5 +1,24 @@
 $(function() {
 
-	// Custom JS
-
+	$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+$(document).ready(function(){
+	$("#m-menu").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+					top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1000);
+	});
+});
+		
+		$('.mobile-menu-button').click(function () {
+			$('.mobile-menu').stop().slideToggle();
+		});
+		
 });
